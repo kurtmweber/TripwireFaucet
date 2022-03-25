edbc10d95e98818c53c46f460bacfc7601bf2632
Kurt Weber <weberk294@gmail.com>
2022-03-25 18:09:51 -0500

    Update checking

--- src/main/java/net/bourboncraft/tripwirefaucet/TripwireFaucet.java
+++ src/main/java/net/bourboncraft/tripwirefaucet/TripwireFaucet.java
@@ -10,6 +10,16 @@ public class TripwireFaucet extends JavaPlugin {
         int pluginId = 14705; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
         getLogger().info("TripwireListener with bStats registered");
+
+        new UpdateChecker(this, 100861).getVersion(version -> {
+            if (this.getDescription().getVersion().equals(version)) {
+                getLogger().info("There is not a new update available.");
+                UpdateChecker.updateAvailable = false;
+            } else {
+                UpdateChecker.updateAvailable = true;
+                getLogger().info("There is a new update available.");
+            }
+        });
     }
 
     @Override
--- src/main/java/net/bourboncraft/tripwirefaucet/TripwireListener.java
+++ src/main/java/net/bourboncraft/tripwirefaucet/TripwireListener.java
@@ -5,15 +5,18 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Levelled;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.jetbrains.annotations.NotNull;
 
 public class TripwireListener implements Listener {
     @EventHandler
-    public void onTripwireClicked(PlayerInteractEvent e){
+    public void onTripwireClicked(@NotNull PlayerInteractEvent e){
         Block b = e.getClickedBlock();
         if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getHand().equals(EquipmentSlot.HAND) && b.getType().equals(Material.TRIPWIRE_HOOK)){
             Block d = b.getRelative(BlockFace.DOWN);
@@ -27,6 +30,14 @@ public class TripwireListener implements Listener {
                 dbd.setLevel(dbd.getMaximumLevel());
                 d.setBlockData(dbd);
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(@NotNull PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        if (p.isOp() && UpdateChecker.updateAvailable){
+            p.sendMessage("There is an update available for TripwireFaucet!");
         }
     }
 }
